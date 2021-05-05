9e1d1f8f9b11a78297759e42421e429747e6e0c7
leamariacamposjahn <leamariacamposjahn@leas-air>
2021-05-05 23:39:24 +0200

    test

--- Test12/src/Test.java
+++ Test12/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hallo test");
+System.out.println("sldsjslcsmlcsmclk test");
 System.out.println("Hallo");
 	}
 
